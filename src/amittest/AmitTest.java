96e53ca7ab050e04b53af21f2a923061ba7c5bfd
admin <admin@sqas-air.lan>
2018-01-28 23:48:20 -0800

    second commit

--- src/amittest/AmitTest.java
+++ src/amittest/AmitTest.java
@@ -16,7 +16,8 @@ public class AmitTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("my test app");
+        System.out.println("my test app, updated");
+        
     }
     
 }
